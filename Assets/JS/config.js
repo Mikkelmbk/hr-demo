af570da66ccb0a214a13dc1fda10416d7d273742
Mikkel <Mikkelmbk@live.dk>
2025-11-14 14:00:33 +0100

    refactored code related to config form handling, to better separate form components

--- Assets/JS/config.js
+++ Assets/JS/config.js
@@ -1,41 +1,50 @@
-let websiteUuidClearButton = document.querySelector(".js-button-clear-localstorage");
-let cForm = document.querySelector('.js-form');
-cForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+let cFormApply = document.querySelector('.js-form-apply');
+let cFormClear = document.querySelector('.js-form-clear');
 
-    let cForm__inputWebsiteUuid = cForm.querySelector('.js-input-websiteUuid');
-    let cForm__response = cForm.querySelector(".js-response");
+let cFormApply__inputWebsiteUuid = cFormApply.querySelector('.js-input-websiteUuid');
+let cFormApply__response = cFormApply.querySelector(".js-response");
+
+let cFormClear__response = cFormClear.querySelector(".js-response");
+
+cFormApply.addEventListener('submit', (e) => {
+    e.preventDefault();
 
     window.hrq = window.hrq || [];
     hrq.push(['init',
         {
-            websiteUuid: cForm__inputWebsiteUuid.value
+            websiteUuid: cFormApply__inputWebsiteUuid.value
         }
     ]);
 
     hrq.push(function () {
-        localStorage.setItem("websiteUuid_from_config", cForm__inputWebsiteUuid.value);
+        localStorage.setItem("websiteUuid_from_config", cFormApply__inputWebsiteUuid.value);
 
-        cForm__response.textContent = `Website Uuid: "${cForm__inputWebsiteUuid.value}" was comitted to localstorage. Hello Retail Script will be initialized with Website Uuid from localstorage on subsequent page navigations, until you overwrite it.`;
+        cFormApply__response.textContent = `Website Uuid: "${cFormApply__inputWebsiteUuid.value}" was comitted to localstorage. Hello Retail Script will be initialized with Website Uuid from localstorage on subsequent page navigations, until you overwrite it.`;
+
+        cFormClear__response.textContent = "";
 
         if (localStorage.getItem("websiteUuid_from_config")) {
-            websiteUuidClearButton.classList.remove("is-hidden");
+            cFormClear.classList.remove("is-hidden");
         }
     });
 
     setTimeout(() => {
-        if (!window.hrq.listenForEmailFields_eventListeners && !cForm__response.textContent) {
-            cForm__response.textContent = `Website Uuid not recognized. Reload the page and try again`;
+        if (!window.hrq.listenForEmailFields_eventListeners && !cFormApply__response.textContent) {
+            cFormApply__response.textContent = `Website Uuid not recognized. Reload the page and try again`;
         }
     }, 750);
 });
 
 if (localStorage.getItem("websiteUuid_from_config")) {
-    websiteUuidClearButton.classList.remove("is-hidden");
+    cFormClear.classList.remove("is-hidden");
 };
 
-websiteUuidClearButton.addEventListener("click",(e)=>{
-    document.querySelector(".js-response").textContent = `Website Uuid ${localStorage.getItem("websiteUuid_from_config")} was removed from localstorage.`;
+cFormClear.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    cFormClear__response.textContent = `Website Uuid ${localStorage.getItem("websiteUuid_from_config")} was removed from localstorage.`;
     localStorage.removeItem("websiteUuid_from_config");
-    websiteUuidClearButton.classList.add("is-hidden");
+
+    cFormApply__response.textContent = "";
+
 });
